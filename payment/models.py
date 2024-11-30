e2f0a7bc6be375425ec7973060aa55735653dd93
mohamed <mos3doun26@gmail.com>
2024-11-30 20:22:33 +0200

    adding shipping date

--- payment/models.py
+++ payment/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
 import datetime
 
 class ShippingAddress(models.Model):
@@ -37,9 +38,21 @@ class Order(models.Model):
     amount_paid = models.DecimalField(max_digits=7, decimal_places=2)
     date_ordered = models.DateTimeField(default=datetime.datetime.now, blank=True)
     shipped = models.BooleanField(default=False)
+    date_shipped = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"Order - {self.id}"
+
+# create a signal to create a shippinge date if the order shipped equel to True
+@receiver(pre_save,sender=Order)
+def set_shipping_date_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        # the the obj (order)
+        obj = sender._default_manager.get(pk=instance.pk)
+        # create shipping time
+        now = datetime.datetime.now()
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
     
 class OrderItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
